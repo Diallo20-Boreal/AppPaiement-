0e852345dc44fb94ca446e532634e0430856b7ef
ALHASSANE DIALLO <alhassane.diallo@monboreal.ca>
2024-11-18 11:36:38 -0500

    classe paiement

---
+++ Paiement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Paiement
+    {
+        // la declaration des paramettres de la classe 
+        public double Montant { get; set; }
+        public string Description { get; set; }
+
+        //le constructeur de la classe
+        public Paiement(double montant, string description)
+        {
+            Montant = montant;
+            Description = description;
+        }
+
+        //la creation de la methode afficher
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"le montant est {Montant} et ça description est {Description}");
+        }
+    }
+}
