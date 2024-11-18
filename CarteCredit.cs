7db6e086e260f01f61706f5bdec79a38232d3752
ALHASSANE DIALLO <alhassane.diallo@monboreal.ca>
2024-11-18 11:43:29 -0500

    Classe fille carte credit

---
+++ CarteCredit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    // Creation de la classe CarteCredit qui herite de la classe Paiement 
+    internal class CarteCredit : Paiement
+    {
+        // la declaration des paramettres de la classe 
+        public int NumeroCarte { get; set; }
+
+        //le constructeur de la classe Carte Credit
+        public CarteCredit(int numeroCarte, double montant, string description) : base(montant, description)
+        {
+            NumeroCarte = numeroCarte;
+        }
+        //la creation de la methode afficher
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"le numéro de la carte est {NumeroCarte},le montant est {Montant} et la description est {Description}.");
+        }
+    }
+}
